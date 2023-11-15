d2475c00eb0ced928b0bb3136174621fc72ec892
sebas980922 <sebastianzzz123456@gmail.com>
2023-11-15 01:38:43 -0500

    ajustes en mobile

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.querySelector('.menu-icon');
+    const closeIcon = document.querySelector('#close-icon');
     const nav = document.querySelector('nav');
+    const body = document.body;
+    const overlay = document.querySelector('.overlay');
 
-    menuIcon.addEventListener('click', function () {
+    function toggleMenu() {
         nav.classList.toggle('active');
-    });
+        body.classList.toggle('menu-active');
+    }
+
+    menuIcon.addEventListener('click', toggleMenu);
+    closeIcon.addEventListener('click', toggleMenu);
+    overlay.addEventListener('click', toggleMenu);
 });

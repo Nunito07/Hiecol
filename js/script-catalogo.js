311e7d1b2748bdcb7d7dd0302d99ddf96d90ef80
Juan Sebastian Zuluaga Leon <jzuluagl@emeal.nttdata.com>
2023-12-04 00:25:46 -0500

    mensaje del commit, se arregló el nav

--- js/script-catalogo.js
+++ js/script-catalogo.js
@@ -1,3 +1,24 @@
+
+
+// Objeto que mapea identificadores de productos con descripciones
+const descripciones = {
+    producto1: "Descripción detallada del Producto 1.",
+    producto2: "Descripción detallada del Producto 2.",
+    // Agrega más productos según sea necesario
+};
+
+// Función para obtener la descripción de un producto
+function obtenerDescripcion(idProducto) {
+    // Verifica si el identificador de producto existe en el objeto descripciones
+    if (descripciones.hasOwnProperty(idProducto)) {
+        // Devuelve la descripción correspondiente
+        return descripciones[idProducto];
+    } else {
+        // Si el identificador no está en el objeto, devuelve un mensaje de error
+        return "Descripción no disponible.";
+    }
+}
+
 function cargar(btn) {
     // Obtener el contenedor del item (padre del botón)
     var item = btn.closest('.item');
@@ -5,13 +26,11 @@ function cargar(btn) {
     // Obtener el id del producto desde el atributo id del contenedor
     var idProducto = item.id;
 
-    // Obtener la descripción detallada del producto según el id
-    var descripcionDetallada = obtenerDescripcionDetallada(idProducto);
+    // Obtener la descripción del producto según el id
+    var descripcionProducto = obtenerDescripcion(idProducto);
 
-    // Clonar el contenido del item excluyendo el botón de descripción
+    // Clonar el contenido del item
     var itemClonado = item.cloneNode(true);
-    var btnDescripcionClonado = itemClonado.querySelector('#btn-description');
-    btnDescripcionClonado.remove(); // Excluir el botón de descripción
 
     // Limpiar el contenido existente del modal-body
     var modalBody = document.querySelector('.modal-body');
@@ -20,29 +39,25 @@ function cargar(btn) {
     // Agregar el item clonado al modal-body
     modalBody.appendChild(itemClonado);
 
-    // Actualizar el contenido del modal con la descripción detallada
-    document.getElementById('descripcionDetallada').innerText = descripcionDetallada;
+    // Actualizar el contenido del modal con la descripción
+    document.getElementById('descripcionProducto').innerText = descripcionProducto;
+
+    // Ajustar el tamaño de la imagen en el modal
+    ajustarTamanioImagenEnModal();
 
     // Abre el modal
     var modal = new bootstrap.Modal(document.getElementById('seleccion'));
     modal.show();
 }
 
-// Objeto que mapea identificadores de productos con descripciones detalladas
-const descripcionesDetalladas = {
-    producto1: "Descripción detallada del Producto 1.",
-    producto2: "Descripción detallada del Producto 2.",
-    // Agrega más productos según sea necesario
-};
+// Función para ajustar el tamaño de la imagen en el modal
+function ajustarTamanioImagenEnModal() {
+    // Selecciona la imagen dentro del modal
+    var imagenEnModal = document.querySelector('.modal-body .item img');
 
-// Función para obtener la descripción detallada de un producto
-function obtenerDescripcionDetallada(idProducto) {
-    // Verifica si el identificador de producto existe en el objeto descripcionesDetalladas
-    if (descripcionesDetalladas.hasOwnProperty(idProducto)) {
-        // Devuelve la descripción detallada correspondiente
-        return descripcionesDetalladas[idProducto];
-    } else {
-        // Si el identificador no está en el objeto, devuelve un mensaje de error
-        return "Descripción detallada no disponible.";
+    // Aplica estilos a la imagen
+    if (imagenEnModal) {
+        imagenEnModal.style.maxWidth = "150px";
+        imagenEnModal.style.height = "auto";
     }
 }
